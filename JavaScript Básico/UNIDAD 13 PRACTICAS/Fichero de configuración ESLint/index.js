dddf72cadf7d9963483501c7824b2da82584b6ae
LuiscaxD <luiscarlosabril44@gmail.com>
2022-10-12 18:11:25 -0600

    Upload 1.05

---
+++ JavaScript Básico/UNIDAD 13 PRACTICAS/Fichero de configuración ESLint/index.js
@@ -0,0 +1,10 @@
+const nombre = "Luisca"
+
+var nombre3 = "Maria"
+
+var objeto = {
+    nombre: "circulo",
+    radio: 2,
+    nombre: "rectángulo"
+}
+
---
+++ JavaScript Básico/UNIDAD 13 PRACTICAS/Instalación y creación de ficheros de configuración/Eslint/index.js
@@ -0,0 +1,14 @@
+const nombre = "Gorka";
+
+// En estas lineas quiero tener comillas simples (que me desactives la regla de las comillas dobles de eslint)
+
+/* eslint-disable */
+const persona2 = 'María'; 
+/* eslint-enable */
+
+const nuevostring = 'esto es un nuevo string'; // eslint-disable-line 
+
+/* eslint-disable-next-line indent */
+    const string2 = "Más strings";
+
+console.log("hola");
