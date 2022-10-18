62dba491bda1660547dcf07ede6ca1bc52abb5a1
LuiscaxD <luiscarlosabril44@gmail.com>
2022-10-18 17:16:10 -0600

    Upload 1.08

---
+++ JavaScript Básico/UNIDAD 16 PRACTICAS/Drag and Drop con JavaScript/index.js
@@ -0,0 +1,32 @@
+const parrafos = document.querySelectorAll(".parrafo");
+const secciones = document.querySelectorAll(".seccion");
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event =>{
+        console.log(`Estoy arrastrando el ${parrafo.innerText}`);
+        parrafo.classList.add("dragging");
+        event.dataTransfer.setData("id", parrafo.id);
+        const elemento_fantasma = document.querySelector(".imagen-fantasma");
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0);
+    })
+    parrafo.addEventListener("dragend", () =>{
+        // console.log(`Se dejo de arrastrar el ${parrafo.innerText}`);
+        parrafo.classList.remove("dragging");
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "move"; // copy por defecto
+        // console.log("DragOver");
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop");
+        const id_parrafo = event.dataTransfer.getData("id");
+        // console.log(`Parrafo id: ${id_parrafo}`);
+        const parrafo = document.getElementById(id_parrafo);
+        seccion.appendChild(parrafo);
+    })
+})
